5681949b285f43d1a3ee88a105501be8a048a77f
imHinotori <hello@hinotori.moe>
2023-11-15 17:27:30 -0300

    Updated Config && Added template file

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,6 +4,7 @@ type Config struct {
 	General General `koanf:"general"`
 	HTTP    HTTP    `koanf:"http"`
 	JWT     JWT     `koanf:"jwt"`
+	Duoc    Duoc    `koanf:"duoc"`
 }
 
 type General struct {
@@ -22,6 +23,15 @@ type JWT struct {
 	PrivateKey   string `koanf:"private-key"`
 	PublicKey    string `koanf:"public-key"`
 	AutoGenerate bool   `koanf:"auto-generate"`
+}
+
+type Duoc struct {
+	SSOURL       string `koanf:"sso-url"`
+	MobileAPIUrl string `koanf:"mobile-api-url"`
+	WebAPIUrl    string `koanf:"web-api-url"`
+	ClientSecret string `koanf:"client-secret"`
+	ClientId     string `koanf:"client-id"`
+	GrantType    string `koanf:"grant-type"`
 }
 
 func Default() *Config {
