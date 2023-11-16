17adcca393e830d6531078dc623ccac669e1090b
imHinotori <hello@hinotori.moe>
2023-11-15 22:13:51 -0300

    ✨ Created Auth Service && added Authentication Endpoint

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,1 +1,119 @@
 package auth
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/charmbracelet/log"
+	"github.com/imhinotori/duoc-plus/internal/config"
+	"github.com/imhinotori/duoc-plus/internal/duoc"
+	"github.com/kataras/iris/v12"
+	"github.com/kataras/iris/v12/middleware/jwt"
+	"net/url"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type Service struct {
+	Config   *config.Config
+	Signer   *jwt.Signer
+	Verifier *jwt.Verifier
+	Duoc     *duoc.Client
+}
+
+func New(cfg *config.Config, signer *jwt.Signer, verifier *jwt.Verifier, duoc *duoc.Client) *Service {
+	return &Service{
+		Config:   cfg,
+		Signer:   signer,
+		Verifier: verifier,
+		Duoc:     duoc,
+	}
+}
+
+type Credentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type Claims struct {
+	Username    string `json:"username"`
+	Email       string `json:"email"`     // Its username + @duocuc.cl
+	StudentCode string `json:"codAlumno"` // It's probably an int, but well.
+	StudentId   int    `json:"idAlumno"`  // Why two ids (?) I don't know.
+	ApiBearer   string `json:"api_bearer"`
+}
+
+func (s Service) Authenticate(credentials Credentials) (jwt.TokenPair, error) {
+	log.Debug("Trying to authenticate user", "username", credentials.Username)
+	endpoint := "/auth/realms/WEB_APPS_PRD/protocol/openid-connect/token"
+
+	data := url.Values{}
+	data.Set("client_id", s.Config.Duoc.ClientId)
+	data.Set("client_secret", s.Config.Duoc.ClientSecret)
+	data.Set("grant_type", "password")
+	data.Set("username", credentials.Username)
+	data.Set("password", credentials.Password)
+
+	response, code, err := s.Duoc.Request(s.Config.Duoc.SSOURL+endpoint, "POST", []byte(data.Encode()), nil)
+
+	if err != nil {
+		return jwt.TokenPair{}, err
+	}
+
+	if code != iris.StatusOK {
+		return jwt.TokenPair{}, fmt.Errorf("invalid response structure: %s", string(response))
+	}
+
+	var ssoResponseData ssoAuthResponse
+
+	if err2 := json.Unmarshal(response, &ssoResponseData); err2 != nil {
+		return jwt.TokenPair{}, err2
+	}
+
+	log.Debug("Successfully authenticated User, Getting some general data", "username", credentials.Username)
+
+	// Get some data....
+
+	endpoint = "/vivo_v1.0/v1/matriculaVigente"
+
+	query := url.Values{}
+	query.Set("nombreUsuario", credentials.Username)
+
+	response, code, err = s.Duoc.RequestWithQuery(s.Config.Duoc.MobileAPIUrl+endpoint, "GET", nil, query, ssoResponseData.AccessToken)
+
+	if err != nil {
+		return jwt.TokenPair{}, err
+	}
+
+	if code != iris.StatusOK {
+		return jwt.TokenPair{}, fmt.Errorf("invalid response structure: %s", string(response))
+	}
+
+	var responseData struct {
+		CodAlumno string `json:"codAlumno"`
+		IDAlumno  int    `json:"idAlumno"`
+		Rut       string `json:"rut"`
+		RutDV     string `json:"rut_dv"`
+	}
+
+	if err2 := json.Unmarshal(response, &responseData); err2 != nil {
+		return jwt.TokenPair{}, err2
+	}
+
+	log.Debug("Successfully got some general data", "username", credentials.Username)
+
+	claims := Claims{
+		Username:    credentials.Username,
+		Email:       strings.Replace(credentials.Username, "@duocuc.cl", "", -1),
+		StudentCode: responseData.CodAlumno,
+		StudentId:   responseData.IDAlumno,
+		ApiBearer:   ssoResponseData.AccessToken,
+	}
+
+	refreshClaims := jwt.Claims{Subject: strconv.Itoa(responseData.IDAlumno)}
+
+	log.Debug("User authenticated successfully, returning Tokens", "username", credentials.Username)
+
+	return s.Signer.NewTokenPair(claims, refreshClaims, 120*time.Hour) // TODO!
+
+}
--- internal/auth/handler.go
+++ internal/auth/handler.go
@@ -1,1 +1,39 @@
 package auth
+
+import (
+	"github.com/kataras/iris/v12"
+)
+
+type Provider interface {
+	Authenticate(ctx iris.Context)
+}
+
+type Handler struct {
+	Service *Service
+}
+
+func (h Handler) Start(app *iris.Application) {
+	party := app.Party("/auth")
+	party.Post("/", h.Authenticate)
+}
+
+func (h Handler) Authenticate(ctx iris.Context) {
+	var creds Credentials
+
+	if err := ctx.ReadBody(&creds); err != nil {
+		ctx.StopWithJSON(iris.StatusBadRequest, iris.Map{
+			"message": "Error reading body",
+		})
+		return
+	}
+
+	tokens, err := h.Service.Authenticate(creds)
+	if err != nil {
+		ctx.StopWithJSON(iris.StatusBadRequest, iris.Map{
+			"message": err.Error(),
+		})
+		return
+	}
+
+	ctx.StopWithJSON(iris.StatusOK, tokens)
+}
