32dea372a39f1030b69e009536f5cd0f62018195
Matías <hello@hinotori.moe>
2023-11-17 20:01:00 -0300

    🎨 Return complete schedule instead of just the first one

--- internal/schedule/schedule.go
+++ internal/schedule/schedule.go
@@ -25,7 +25,7 @@ func New(cfg *config.Config, duoc *duoc.Client) *Service {
 	}
 }
 
-func (s Service) Schedule(claims *auth.Claims) ([]common.Day, error) {
+func (s Service) Schedule(claims *auth.Claims) ([]common.Schedule, error) {
 	endpoint := "/horario_v1.0/v1/horario"
 
 	query := url.Values{}
@@ -34,20 +34,20 @@ func (s Service) Schedule(claims *auth.Claims) ([]common.Day, error) {
 	response, code, err := s.Duoc.RequestWithQuery(s.Config.Duoc.MobileAPIUrl+endpoint, "GET", nil, query, claims.DuocApiBearerToken)
 
 	if err != nil {
-		return []common.Day{}, err
+		return []common.Schedule{}, err
 	}
 
 	if code != iris.StatusOK {
-		return []common.Day{}, fmt.Errorf("invalid response structure: %s", string(response))
+		return []common.Schedule{}, fmt.Errorf("invalid response structure: %s", string(response))
 	}
 
-	var responseData common.Schedule
+	var responseData []common.Schedule
 
 	if err = json.Unmarshal(response, &responseData); err != nil {
-		return []common.Day{}, err
+		return []common.Schedule{}, err
 	}
 
 	log.Debug("Getting schedule data", "username", claims.Username)
 
-	return responseData.Days, nil
+	return responseData, nil
 }
