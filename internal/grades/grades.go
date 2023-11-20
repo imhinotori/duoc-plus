3c1cfd66ba8562a6b985423b7d6f6a290461c095
Matías <hello@hinotori.moe>
2023-11-19 22:37:14 -0300

    ✨ New Grade Service

---
+++ internal/grades/grades.go
@@ -0,0 +1,52 @@
+package grades
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/charmbracelet/log"
+	"github.com/imhinotori/duoc-plus/internal/auth"
+	"github.com/imhinotori/duoc-plus/internal/common"
+	"github.com/imhinotori/duoc-plus/internal/config"
+	"github.com/imhinotori/duoc-plus/internal/duoc"
+	"github.com/kataras/iris/v12"
+	"net/url"
+)
+
+type Service struct {
+	Config *config.Config
+	Duoc   *duoc.Client
+}
+
+func New(cfg *config.Config, duoc *duoc.Client) *Service {
+	return &Service{
+		Config: cfg,
+		Duoc:   duoc,
+	}
+}
+
+func (s Service) Grades(claims *auth.Claims) ([]common.GradesCourses, error) {
+	endpoint := "/notas_v1.0/v1/notasAlumno"
+
+	query := url.Values{}
+	query.Set("codAlumno", claims.StudentCode)
+
+	response, code, err := s.Duoc.RequestWithQuery(s.Config.Duoc.MobileAPIUrl+endpoint, "GET", nil, query, claims.DuocApiBearerToken)
+
+	if err != nil {
+		return []common.GradesCourses{}, err
+	}
+
+	if code != iris.StatusOK {
+		return []common.GradesCourses{}, fmt.Errorf("invalid response structure: %s", string(response))
+	}
+
+	var responseData []common.GradesCourses
+
+	if err = json.Unmarshal(response, &responseData); err != nil {
+		return responseData, err
+	}
+
+	log.Debug("Getting grades data", "username", claims.Username)
+
+	return responseData, nil
+}
