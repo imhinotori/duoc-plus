b3d08f0c8698c4c92f1cdcd2a522f4c5c33bcb88
Matías <hello@hinotori.moe>
2023-11-25 14:10:09 -0300

    🎨 Schedule now return new structure

--- internal/schedule/schedule.go
+++ internal/schedule/schedule.go
@@ -9,8 +9,11 @@ import (
 	"github.com/imhinotori/duoc-plus/internal/config"
 	"github.com/imhinotori/duoc-plus/internal/duoc"
 	"github.com/kataras/iris/v12"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
 	"net/url"
 	"strconv"
+	"strings"
 )
 
 type Service struct {
@@ -25,7 +28,7 @@ func New(cfg *config.Config, duoc *duoc.Client) *Service {
 	}
 }
 
-func (s Service) Schedule(claims *auth.Claims) ([]common.DuocSchedule, error) {
+func (s Service) Schedule(claims *auth.Claims) ([]common.CareerSchedule, error) {
 	endpoint := "/horario_v1.0/v1/horario"
 
 	query := url.Values{}
@@ -34,20 +37,74 @@ func (s Service) Schedule(claims *auth.Claims) ([]common.DuocSchedule, error) {
 	response, code, err := s.Duoc.RequestWithQuery(s.Config.Duoc.MobileAPIUrl+endpoint, "GET", nil, query, claims.DuocApiBearerToken)
 
 	if err != nil {
-		return []common.DuocSchedule{}, err
+		return []common.CareerSchedule{}, err
 	}
 
 	if code != iris.StatusOK {
-		return []common.DuocSchedule{}, fmt.Errorf("invalid response structure: %s", string(response))
+		return []common.CareerSchedule{}, fmt.Errorf("invalid response structure: %s", string(response))
 	}
 
 	var responseData []common.DuocSchedule
 
 	if err = json.Unmarshal(response, &responseData); err != nil {
-		return []common.DuocSchedule{}, err
+		return []common.CareerSchedule{}, err
 	}
 
 	log.Debug("Getting schedule data", "username", claims.Username)
 
-	return responseData, nil
+	caser := cases.Title(language.LatinAmericanSpanish)
+	schedule := convertDuocScheduleToSchedule(responseData, caser)
+
+	return schedule, nil
+}
+
+func convertDuocScheduleToSchedule(s []common.DuocSchedule, caser cases.Caser) []common.CareerSchedule {
+	schedule := make([]common.CareerSchedule, len(s))
+
+	for i, career := range s {
+		schedule[i].CareerName = strings.Replace(caser.String(career.NameCareer), "  ", " ", -1)
+		schedule[i].Schedule = common.Week{}
+
+		for _, day := range career.Days {
+			course := convertDuocCourseToCourse(day.Courses, caser)
+
+			switch day.Day {
+			case "1":
+				schedule[i].Schedule.Monday = course
+			case "2":
+				schedule[i].Schedule.Tuesday = course
+			case "3":
+				schedule[i].Schedule.Wednesday = course
+			case "4":
+				schedule[i].Schedule.Thursday = course
+			case "5":
+				schedule[i].Schedule.Friday = course
+			case "6":
+				schedule[i].Schedule.Saturday = course
+			case "7":
+				schedule[i].Schedule.Sunday = course
+			}
+
+		}
+	}
+
+	return schedule
+
+}
+
+func convertDuocCourseToCourse(c []common.DuocCourse, caser cases.Caser) []common.Course {
+	course := make([]common.Course, 0, len(c))
+
+	for _, item := range c {
+		course = append(course, common.Course{
+			SubjectName: strings.Replace(caser.String(item.Name), "  ", " ", -1),
+			SubjectCode: item.Code,
+			Instructor:  strings.Replace(caser.String(item.Instructor), "  ", " ", -1),
+			StartTime:   item.StartTime,
+			EndTime:     item.EndTime,
+			Classroom:   item.Classroom,
+		})
+	}
+
+	return course
 }
