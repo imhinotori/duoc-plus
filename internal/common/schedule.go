11376e517d637b40f91af7700140c6491b274e4e
Matías <hello@hinotori.moe>
2023-11-25 14:08:42 -0300

    ✨ New Courses structures

--- internal/common/schedule.go
+++ internal/common/schedule.go
@@ -1,1 +1,25 @@
 package common
+
+type Course struct {
+	SubjectName string `json:"subject_name"`
+	SubjectCode string `json:"subject_code"`
+	Instructor  string `json:"instructor"`
+	StartTime   string `json:"start_time"`
+	EndTime     string `json:"end_time"`
+	Classroom   string `json:"classroom"`
+}
+
+type Week struct {
+	Monday    []Course `json:"monday"`
+	Tuesday   []Course `json:"tuesday"`
+	Wednesday []Course `json:"wednesday"`
+	Thursday  []Course `json:"thursday"`
+	Friday    []Course `json:"friday"`
+	Saturday  []Course `json:"saturday"`
+	Sunday    []Course `json:"sunday"`
+}
+
+type CareerSchedule struct {
+	CareerName string `json:"career_name"`
+	Schedule   Week   `json:"schedule"`
+}
