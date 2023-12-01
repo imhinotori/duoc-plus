c93dc31a078c8fbc0032c8529a7cb9a1d612dd5e
Matías <hello@hinotori.moe>
2023-12-01 13:25:35 -0300

    ✨ New Attendance structure

--- internal/common/attendance.go
+++ internal/common/attendance.go
@@ -1,1 +1,23 @@
 package common
+
+import "time"
+
+type AttendanceDetail struct {
+	Date       time.Time `json:"date"`
+	Attendance int       `json:"attendance"`
+}
+
+type SubjectAttendance struct {
+	Name            string             `json:"name"`
+	Code            string             `json:"code"`
+	ClassesHeld     int                `json:"classes_held"`
+	AssistedClasses int                `json:"assisted_classes"`
+	Percentage      float64            `json:"percentage"`
+	Details         []AttendanceDetail `json:"details"`
+}
+
+type Attendance struct {
+	DegreeName        string              `json:"degree_name"`
+	DegreeCode        string              `json:"degree_code"`
+	SubjectAttendance []SubjectAttendance `json:"attendance"`
+}
