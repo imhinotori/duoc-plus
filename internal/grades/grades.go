e95e1178d313e491a5e4bfdabe1406b436e14303
Matías <hello@hinotori.moe>
2023-12-01 16:05:15 -0300

    ✨ Grades function now uses the new structure

--- internal/grades/grades.go
+++ internal/grades/grades.go
@@ -8,6 +8,7 @@ import (
 	"github.com/imhinotori/duoc-plus/internal/common"
 	"github.com/imhinotori/duoc-plus/internal/config"
 	"github.com/imhinotori/duoc-plus/internal/duoc"
+	"github.com/imhinotori/duoc-plus/internal/helper"
 	"github.com/kataras/iris/v12"
 	"net/url"
 )
@@ -24,7 +25,7 @@ func New(cfg *config.Config, duoc *duoc.Client) *Service {
 	}
 }
 
-func (s Service) Grades(claims *auth.Claims) ([]common.DuocGradesCourses, error) {
+func (s Service) Grades(claims *auth.Claims) ([]common.Grades, error) {
 	endpoint := "/notas_v1.0/v1/notasAlumno"
 
 	query := url.Values{}
@@ -33,20 +34,64 @@ func (s Service) Grades(claims *auth.Claims) ([]common.DuocGradesCourses, error)
 	response, code, err := s.Duoc.RequestWithQuery(s.Config.Duoc.MobileAPIUrl+endpoint, "GET", nil, query, claims.DuocApiBearerToken)
 
 	if err != nil {
-		return []common.DuocGradesCourses{}, err
+		return []common.Grades{}, err
 	}
 
 	if code != iris.StatusOK {
-		return []common.DuocGradesCourses{}, fmt.Errorf("invalid response structure: %s", string(response))
+		return []common.Grades{}, fmt.Errorf("invalid response structure: %s", string(response))
 	}
 
 	var responseData []common.DuocGradesCourses
 
 	if err = json.Unmarshal(response, &responseData); err != nil {
-		return responseData, err
+		return []common.Grades{}, err
 	}
 
 	log.Debug("Getting grades data", "username", claims.Username)
 
-	return responseData, nil
+	grades := make([]common.Grades, 0, len(responseData))
+
+	for i, data := range responseData {
+		grades = append(grades, convertDuocGradesToGrades(data))
+
+		log.Debug("Getting grades data", "username", claims.Username, "course", i, "courseName", data.DegreeName)
+	}
+
+	return grades, nil
+}
+
+func convertDuocGradesToGrades(original common.DuocGradesCourses) common.Grades {
+	grades := common.Grades{
+		Name: original.DegreeName,
+		Code: original.DegreeCode,
+	}
+
+	for _, subject := range original.Subjects {
+		newSubject := common.Subject{
+			Code:     subject.SubjectCode,
+			Name:     subject.Name,
+			Average:  helper.ConvertToFloat64(subject.Average),
+			Partials: make([]float64, 0, len(subject.PartialGrades)),
+			Exams:    make([]float64, 0, len(subject.ExamsGrades)),
+		}
+
+		for _, grade := range subject.PartialGrades {
+			if grade.Text == "Nota Final" || grade.Text == "Nota Presentación" {
+				continue
+			}
+
+			newSubject.Partials = append(newSubject.Partials, helper.ConvertToFloat64(grade.Grade))
+		}
+
+		for _, grade := range subject.ExamsGrades {
+			if grade.Text == "Nota Final" || grade.Text == "Nota Presentación" {
+				continue
+			}
+			newSubject.Exams = append(newSubject.Exams, helper.ConvertToFloat64(grade.Grade))
+		}
+
+		grades.Subjects = append(grades.Subjects, newSubject)
+	}
+
+	return grades
 }
