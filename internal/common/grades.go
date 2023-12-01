fade54c2fc6e0dc7a9a15724d20aadb065ce0ba8
Matías <hello@hinotori.moe>
2023-12-01 16:04:48 -0300

    ✨ Create Grades structures

--- internal/common/grades.go
+++ internal/common/grades.go
@@ -1,1 +1,15 @@
 package common
+
+type Grades struct {
+	Name     string    `json:"name"`
+	Code     string    `json:"code"`
+	Subjects []Subject `json:"subjects"`
+}
+
+type Subject struct {
+	Code     string    `json:"code"`
+	Name     string    `json:"name"`
+	Average  float64   `json:"average"`
+	Partials []float64 `json:"partials"`
+	Exams    []float64 `json:"exams"`
+}
