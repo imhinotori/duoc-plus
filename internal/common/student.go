829b9467caa02a915e6232488a2336f9f97bbea1
imhinotori <hello@hinotori.moe>
2024-04-08 23:19:48 -0400

    🎨 Student endpoint now omits empty values (on response).

--- internal/common/student.go
+++ internal/common/student.go
@@ -12,13 +12,13 @@ type User struct {
 	Rut         string   `json:"rut"`
 	Avatar      string   `json:"avatar"`
 	Careers     []Career `json:"careers"`
-	Email       string   `json:"email"`
-	Username    string   `json:"username"`
-	StudentCode string   `json:"codAlumno"` // It's probably an int, but well.
-	StudentId   int      `json:"idAlumno"`  // Why two ids (?) I don't know.
+	Email       string   `json:"email,omitempty"`
+	Username    string   `json:"username,omitempty"`
+	StudentCode string   `json:"codAlumno,omitempty"` // It's probably an int, but well.
+	StudentId   int      `json:"idAlumno,omitempty"`  // Why two ids (?) I don't know.
 	//
-	AccessToken           string `json:"access_token"`
-	AccessTokenExpiresIn  int    `json:"access_token_expires_in"`
-	RefreshToken          string `json:"refresh_token"`
-	RefreshTokenExpiresIn int    `json:"refresh_expires_in"`
+	AccessToken           string `json:"access_token,omitempty"`
+	AccessTokenExpiresIn  int    `json:"access_token_expires_in,omitempty"`
+	RefreshToken          string `json:"refresh_token,omitempty"`
+	RefreshTokenExpiresIn int    `json:"refresh_expires_in,omitempty"`
 }
